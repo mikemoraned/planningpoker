7dd42edb8864bbe15fc042559cf3a7fa24cdf7fd
Mike Moran <mike@houseofmoran.com>
2014-05-05 22:42:50 +0100

    switch to ejs for templates

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ var express = require('express')
   , routes = require('./routes')
 
 var app = module.exports = express.createServer();
+var io = require('socket.io').listen(app);
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('view engine', 'ejs');
+    app.set('views',__dirname + '/views');
+    app.set('view options', { layout:false, root: __dirname + '/views' });
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
