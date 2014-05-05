bededa78eee87314652378a1f743cd2a60764f8c
Mike Moran <mike@houseofmoran.com>
2014-05-05 22:19:52 +0100

    add display and vote pages

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,19 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+    res.render('index', { title: 'Express' });
+};
+
+exports.display = function(req, res){
+    var room = req.params["room"];
+    console.log(room);
+    res.render('display', { title: 'Display' });
+};
+
+exports.vote = function(req, res){
+    var room = req.params["room"];
+    var user = req.params["user"];
+    console.log(room);
+    console.log(user);
+    res.render('vote', { title: 'Vote' });
 };
